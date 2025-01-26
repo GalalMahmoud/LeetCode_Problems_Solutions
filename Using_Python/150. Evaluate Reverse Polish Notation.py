b17e57ea2f49eef9f49b9aff475b79d3899217e2
GalalMahmoud <galal_mahmoud2025@yahoo.com>
2025-01-26 09:19:50 +0200

    150 Python

---
+++ Using_Python/150. Evaluate Reverse Polish Notation.py
@@ -0,0 +1,24 @@
+from typing import List
+
+# My solution
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        stack = []
+        res = tokens[0]
+        for t in tokens:
+            if t.lstrip('-').isdigit(): stack.append(t)
+            else:
+                match t:
+                    case '+': 
+                        res = int(stack[-1])+int(stack[-2])
+                    case '-':
+                        res = int(stack[-2])-int(stack[-1])
+                    case '*':
+                        res = int(stack[-1])*int(stack[-2])
+                    case '/':
+                        if int(stack[-1]) != 0:
+                            res = int(stack[-2])/int(stack[-1])
+                        else: res = 0
+                stack = stack[:-2]
+                stack.append(res)
+        return int(res)
