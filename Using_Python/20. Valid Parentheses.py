304e86c073f6c1c70f43996bea02de2272525d49
GalalMahmoud <galal_mahmoud2025@yahoo.com>
2025-02-03 12:11:00 +0200

    20 Python Update

--- Using_Python/20. Valid Parentheses.py
+++ Using_Python/20. Valid Parentheses.py
@@ -1,21 +1,36 @@
-def isValid(s: str)->bool:
-    stack=[]
-    open_bracs ="{[("
-    close_bracs ="}])"
-    last_open = ""
-    for i in range(len(s)):
-        stack.append(s[i])
-        if last_open=="" and s[i] in close_bracs:
-            return False
-        elif s[i] in open_bracs:
-            last_open = s[i]
-        elif s[i] in close_bracs:
-            if open_bracs.index(last_open) != close_bracs.index(s[i]):
+class Solution:
+    def isValid(self, s: str)->bool:
+        stack=[]
+        open_bracs ="{[("
+        close_bracs ="}])"
+        last_open = ""
+        for i in range(len(s)):
+            stack.append(s[i])
+            if last_open=="" and s[i] in close_bracs:
                 return False
-            elif open_bracs.index(last_open) == close_bracs.index(s[i]):
-                stack.pop()
-                stack.pop()
-                if len(stack)>0: last_open = stack[-1]
-                else: last_open = ""
-    return len(stack)==0
+            elif s[i] in open_bracs:
+                last_open = s[i]
+            elif s[i] in close_bracs:
+                if open_bracs.index(last_open) != close_bracs.index(s[i]):
+                    return False
+                elif open_bracs.index(last_open) == close_bracs.index(s[i]):
+                    stack.pop()
+                    stack.pop()
+                    if len(stack)>0: last_open = stack[-1]
+                    else: last_open = ""
+        return len(stack)==0
 
+# حل آخر
+# Another solution
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stk = []
+        opcls = {'[':']',
+                '{':'}',
+                '(':')'}
+        for b in s:
+            if b in opcls.keys(): stk.append(b)
+            else: 
+                if len(stk) == 0 or b != opcls[stk[-1]]:return False
+                else: stk.pop()
+        return True if len(stk)==0 else False
