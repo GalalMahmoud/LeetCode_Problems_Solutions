2598d0b5dc68f7781b163792e4526def8dea1ef0
GalalMahmoud <galal_mahmoud2025@yahoo.com>
2025-02-10 00:06:25 +0200

    84 Python

--- Using_Python/84. Largest Rectangle in Histogram.py
+++ Using_Python/84. Largest Rectangle in Histogram.py
@@ -19,3 +19,55 @@ class Solution:
         return res
     
 
+# أسرع حل (ليس حلي)
+# Fastest solution (not mine)
+__import__("atexit").register(lambda: open("display_runtime.txt", "w").write("0"))
+
+
+class Solution:
+    def largestRectangleArea(self, heights: List[int]) -> int:
+        L = []
+        stackL = []
+        for i in range(len(heights)):
+            if(len(stackL) == 0):
+                L.append(i)
+                stackL.append(i)
+                continue
+                
+            if(heights[stackL[-1]] < heights[i]):
+                L.append(i)
+            else:
+                while len(stackL) > 0 and heights[stackL[-1]] >= heights[i]:
+                    stackL.pop()
+                if(len(stackL)):
+                    L.append(stackL[-1]+1)
+                else:
+                    L.append(0)
+            stackL.append(i)
+        print(L)
+        R = []
+        stackR = []
+        for i in range(len(heights)-1, -1, -1):
+            if(len(stackR) == 0):
+                R.append(i)
+                stackR.append(i)
+                continue
+                
+            if(heights[stackR[-1]] < heights[i]):
+                R.append(i)
+            else:
+                while len(stackR) > 0 and heights[stackR[-1]] >= heights[i]:
+                    stackR.pop()
+                if(len(stackR) == 0):
+                    R.append(len(heights)-1)
+                else:
+                    R.append(stackR[-1]-1)
+
+            stackR.append(i)
+        R.reverse()
+        print(R)
+        ans = 0
+        for i in range(len(heights)):
+            ans = max(ans, heights[i] * (R[i]-L[i]+1))
+            
+        return ans
