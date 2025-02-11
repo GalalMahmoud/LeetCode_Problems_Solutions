034897a5f9b3e9327c37d553871495b9bf9f702f
GalalMahmoud <galal_mahmoud2025@yahoo.com>
2025-02-12 01:42:58 +0200

    69 Python Update

--- Using_Python/69. Sqrt(x).py
+++ Using_Python/69. Sqrt(x).py
@@ -5,3 +5,22 @@ class Solution:
             if i**2 > x:
                 return i-1
             i+=1
+
+
+# ===================================
+# أفضل حل (ليس حلي) - يستخدم البحث الثنائي
+# Best Solution (not mine) -> using binary search
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        if x<2: return x
+        l, r = 1, x//2
+        while l<=r:
+            mid = (l+r)//2
+            sq = mid * mid
+            if sq==x:
+                return mid
+            elif sq < x:
+                l = mid +1
+            else:
+                r = mid-1
+        return r
