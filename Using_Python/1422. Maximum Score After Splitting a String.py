e7e4a6def22a97c0de3d0360e893d4955abbd2a9
GalalMahmoud <galal_mahmoud2025@yahoo.com>
2025-02-18 10:29:39 +0200

    1422 Python Update

--- Using_Python/1422. Maximum Score After Splitting a String.py
+++ Using_Python/1422. Maximum Score After Splitting a String.py
@@ -14,3 +14,19 @@ class Solution:
             current+=arr[i][1].count('1')
             if current>counter: counter=current
         return counter
+
+# =============================================
+# أفضل حل (ليس حلي)
+# Best solution (not mine)
+class Solution:
+    def maxScore(self, s: str) -> int:
+        left = 0
+        right = 0
+        ans = 0
+        for i in range(len(s)):
+            right += 0 if s[i]=='0' else 1;
+        for i in range(len(s)-1):
+            left += 1 if s[i]=='0' else 0;
+            right += 0 if s[i]=='0' else -1;
+            ans = max(left+right,ans)
+        return ans
