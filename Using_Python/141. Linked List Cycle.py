5d6e063c8719fb881e331548c1800293b9cb6b04
GalalMahmoud <galal_mahmoud2025@yahoo.com>
2025-02-21 01:02:15 +0200

    141 Python Update

--- Using_Python/141. Linked List Cycle.py
+++ Using_Python/141. Linked List Cycle.py
@@ -10,11 +10,29 @@ class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
         if head and head.next:
             next_ = head.next
-            arr = set()
+            arr = []
             while next_.next:
                 if next_ in arr: return True
                 arr.append(next_)
                 next_ = next_.next
         return False
     
-# =======================================================
+# ======================================================= 
+# حل أسرع
+# Faster Solution
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        if head and head.next:
+            next_ = head.next
+            hset = set()
+            while next_.next:
+                if next_ in hset: return True
+                hset.add(next_)
+                next_ = next_.next
+        return False
