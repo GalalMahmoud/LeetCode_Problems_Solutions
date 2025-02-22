ab865bba4254658ac1ac4ab2084cec4966e0d909
GalalMahmoud <galal_mahmoud2025@yahoo.com>
2025-02-23 00:30:18 +0200

    2190 Python Update

--- Using_Python/2190. Most Frequent Number Following Key In an Array.py
+++ Using_Python/2190. Most Frequent Number Following Key In an Array.py
@@ -12,3 +12,18 @@ class Solution:
                 if max_repeat[1]<=t[nums[i+1]]:
                     max_repeat = [nums[i+1],t[nums[i+1]]]
         return max_repeat[0]
+    
+# ====================================
+# أفضل حل
+# Best solution
+class Solution:
+    def mostFrequent(self, nums: List[int], key: int) -> int:
+        m = {}
+        for i in range(len(nums)-1):
+            if nums[i] == key: 
+                m[nums[i+1]] = m.get(nums[i+1], 0)+1
+        resVal, resN = 0, 0
+        for target in m:
+            if m[target] > resN:
+                resN, resVal = m[target], target
+        return resVal
