f79e3fd2b7c9b6bdf793d8b7bdd942e8848c5b9e
GalalMahmoud <galal_mahmoud2025@yahoo.com>
2025-03-01 03:42:18 +0200

    138 Python

---
+++ Using_Python/138. Copy List with Random Pointer.py
@@ -0,0 +1,19 @@
+import copy
+from typing import Optional
+
+class Node:
+        def __init__(self, x: int=0, next: 'Node' = None, random: 'Node' = None):
+            self.val = int(x)
+            self.next = next
+            self.random = random
+
+
+
+
+class Solution:
+    def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
+        return copy.deepcopy(head)
+    
+
+
+
