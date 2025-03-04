5234f069dd99b090c78a62c11a2a1bfbf3cb83a2
GalalMahmoud <galal_mahmoud2025@yahoo.com>
2025-03-04 03:21:04 +0200

    739 Python Update

--- Using_Python/739. Daily Temperatures.py
+++ Using_Python/739. Daily Temperatures.py
@@ -12,3 +12,19 @@ class Solution:
         return res
 
 
+#=====================================================================
+# أفضل حل (ليس حلي)
+# Best solution (not mine)
+__import__("atexit").register(lambda: open("display_runtime.txt", "w").write("0"))
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        stack = [] # Stores index, temps as (i, temp)
+        for i, temp in enumerate(temperatures):
+            while stack and temp > stack[-1][1]:
+                prev_i, _ = stack.pop()
+                temperatures[prev_i] = i - prev_i
+            stack.append((i, temp))
+        while stack:
+            prev_i, _ = stack.pop()
+            temperatures[prev_i] = 0
+        return temperatures
