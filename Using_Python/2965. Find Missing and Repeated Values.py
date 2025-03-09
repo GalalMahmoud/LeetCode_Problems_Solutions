aebfe9bcebcc78ab7e2f98e21bfe253b33d86755
GalalMahmoud <galal_mahmoud2025@yahoo.com>
2025-03-09 03:37:40 +0200

    2965 Python Update

--- Using_Python/2965. Find Missing and Repeated Values.py
+++ Using_Python/2965. Find Missing and Repeated Values.py
@@ -12,3 +12,20 @@ class Solution:
             if v==2: res[0] = k
             elif v==0: res[1] = k
         return res
+    
+# =============================================
+# حل آخر (ليس حلي)
+# Another solution (not mine)
+class Solution:
+    def findMissingAndRepeatedValues(self, grid: List[List[int]]) -> List[int]:
+        n = len(grid)
+        currSum = (n*n)*((n*n) +1)//2
+        seen = set()
+        for i in range(n):
+            for j in range(n):
+                if grid[i][j] not in seen:
+                    seen.add(grid[i][j])
+                    currSum -= grid[i][j]
+                else:
+                    dup = grid[i][j]
+        return [dup, currSum]
